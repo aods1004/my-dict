c159f1dbe6a8490f41888cc0dcca9dca20fa565c
amemura <aods1004@yahoo.co.jp>
2020-09-06 04:27:28 +0900

    update

--- ScriptAutoRunner/youtube.js
+++ ScriptAutoRunner/youtube.js
@@ -1,15 +1,15 @@
+let currentUrl = "";
 if (window.location.href.match(/^https:\/\/www\.youtube\.com\/watch\?v=/)) {
-    if (window.location.href.length > 43) {
-        setCanonicalLinkTag(window.location.href.substr(0, 43));
-    }
+    setCanonicalLinkTag(window.location.href.substr(0, 43));
+} else {
+    setCanonicalLinkTag(window.location.href);
 }
-
 const observer = new MutationObserver((mutations) => {
     mutations.forEach(() => {
         if (window.location.href.match(/^https:\/\/www\.youtube\.com\/watch\?v=/)) {
-            if (window.location.href.length > 43) {
-                setCanonicalLinkTag(window.location.href.substr(0, 43));
-            }
+            setCanonicalLinkTag(window.location.href.substr(0, 43));
+        } else {
+            setCanonicalLinkTag(window.location.href);
         }
     });
 });
@@ -19,16 +19,19 @@ observer.observe(
     {attributes: true, childList: true, characterData: true, subtree: true});
 
 function setCanonicalLinkTag(url) {
-    let links = document.getElementsByTagName("link");
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].rel === "canonical") {
-            links[i].rel = "canonical-back";
+    if (currentUrl !== url) {
+        let links = document.getElementsByTagName("link");
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].rel === "canonical") {
+                links[i].remove();
+            }
         }
+        console.log("canonical uri を「" + url + "」に変更しました");
+        let canonicalLinkTag = window.document.createElement('link');
+        canonicalLinkTag.rel = "canonical";
+        canonicalLinkTag.href = url;
+        window.document.head.append(canonicalLinkTag);
+        currentUrl = url;
     }
-    console.log("canonical uri を「" + url + "」に変更しました");
-    let canonicalLinkTag = window.document.createElement('link');
-    canonicalLinkTag.rel = "canonical";
-    canonicalLinkTag.href = url;
-    window.document.head.append(canonicalLinkTag);
 }
 
