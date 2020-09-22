b89c289e4455f70aeb6253bae9101b0435f6df4d
amemura <aods1004@yahoo.co.jp>
2020-09-22 15:10:19 +0900

    update

--- ScriptAutoRunner/query_remover.js
+++ ScriptAutoRunner/query_remover.js
@@ -1,5 +1,6 @@
 let match;
 setInterval(function () {
+    console.log("check url...");
     if (window.location.href.match(/^https?:\/\/[^?]+\?.*$/)) {
         match = window.location.href.match(/^(https:\/\/[^?]+)\?.*$/)[1];
         history.replaceState('','', match);
